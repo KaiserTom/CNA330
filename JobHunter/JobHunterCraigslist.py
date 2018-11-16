53cd7092bbf1ef576e3774d1c9c311e7e7159c60
KaiserTom <jayt444@comcast.net>
2018-11-15 19:02:29 -0800

    Update JobHunterCraigslist.py

--- JobHunter/JobHunterCraigslist.py
+++ JobHunter/JobHunterCraigslist.py
@@ -3,10 +3,10 @@
 # Zachary Rubin, zrubin@rtc.edu
 import mysql.connector
 import sys
-import json
 import requests
 import os
 import time
+from bs4 import BeautifulSoup
 
 # Connect to database
 # You should not need to edit anything in this function
@@ -20,90 +20,68 @@ def connect_to_sql():
 def create_tables(cursor, table):
     ## Add your code here. Starter code below
     cursor.execute('''CREATE TABLE IF NOT EXISTS Jobs (
-		ID int NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-		PostDate TEXT, 
-		Title TEXT, 
-		Location TEXT, 
-		Description TEXT, 
-		Company TEXT, 
-		Apply_info TEXT, 
-		Salary FLOAT, 
-		RawMessage TEXT, 
-		Craig_ID TEXT UNIQUE); ''')
+        ID int NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+        PostDate TEXT, 
+        Title TEXT, 
+        Location TEXT, 
+        Description TEXT, 
+        Company TEXT, 
+        Apply_info TEXT, 
+        Salary FLOAT, 
+        RawMessage TEXT, 
+        Craig_ID VARCHAR(12) UNIQUE); ''')
     return
 
-# Query the database.
-# You should not need to edit anything in this function
-def query_sql(cursor, query):
-    cursor.execute(query)
-    return cursor
-
 # Add a new job
-def add_new_job(cursor, jobdetails):
-    query = """INSERT IGNORE INTO Jobs (PostDate, Title, Location, Description, Company, Apply_info, Craig_ID) VALUES (
-	"''' + jobdetails['created_at'] + '''",
-	"''' + jobdetails['title'] + '''",
-	"''' + jobdetails['location'] + '''",
-	"''' + jobdetails['description'] + '''",
-	"''' + jobdetails['company'] + '''",
-	"''' + jobdetails['how_to_apply'] + '''",
-	"''' + jobdetails['id'] + '''")"""
-    return query_sql(cursor, query)
-
+insert_stmt = (
+    "INSERT IGNORE INTO Jobs (PostDate, Title, Location, Description, Apply_info, Craig_ID) "
+    "VALUES (%(created_at)s, %(title)s, %(location)s, %(description)s, %(how_to_apply)s, %(id)s)"
+)
 # Check if new job
-def check_if_job_exists(cursor, jobdetails):
-    query = """SELECT EXISTS(SELECT * FROM Jobs WHERE jobdetails['id'])"""
-    return query_sql(cursor, query)
-
+check_stmt = """SELECT EXISTS(SELECT * FROM Jobs WHERE %(id)s)"""
 # Delete given job
-def delete_job(cursor, jobdetails):
-    query = """DELETE FROM Jobs WHERE Craig_ID=jobdetails['id']"""
-    return query_sql(cursor, query)
+delete_stmt = """DELETE FROM Jobs WHERE Craig_ID = %(id)s"""
 
 # Grab new jobs from a website
-def fetch_new_jobs(cursor, arg_dict, url):
-    r = requests.get(url) #Get page
-    soup = BeautifulSoup(r.text, 'html.parser')
-    job_links = soup.find_all('li', class_='result-row') #Find all job listings
-	for job in job_links: #Get single job
-		jobdetails['id'] = job['data-pid']
-		jobdetails['title'] = job.p.a.string #Get title of job
-		jobdetails['created_at'] = job.time['datetime'] #Get date posted of job
-		
-		job_location = job.p.a.next_sibling.next_sibling.find('span', class_='result-hood') #Get location of job
-		try:
-			jobdetails['location'] = job_location.replace('(', '').replace(')', '') #Clean output
-		except:
-			jobdetails['location'] = "No location given"
-		
-		joburl = job.a['href'] #Get url for job
-		jobdetails['how_to_apply'] = joburl
-		
-		jobr = requests.get(joburl) #Delve into each job link and get compensation and description
-		jobsoup = BeautifulSoup(jobr.text, 'html.parser')
-		jobdetails['salary'] = jobsoup.find('p', class_='attrgroup').span.text #Get compensation
-		
-		jobdesc = jobsoup.find('section', id='postingbody') #Get description of job and clean output
-		jobdetails['description'] = jobdesc.text.replace('\n', ' ').replace('\t', ' ')[30:] #[30:] cleans the first 30 characters referencing a QR code
-		
-	return jobdetails
-	
-# Load a text-based configuration file
-def load_config_file(filename):
-    argument_dictionary = []
-    # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/FileIO.py
-    rel_path = os.path.abspath(os.path.dirname(__file__))
-    file = 0
-    file_contents = 0
+def fetch_new_job(arg_dict, job, jobdetails):
+    jobdetails['title'] = str(job.p.a.string) #Get title of job
+    jobdetails['created_at'] = str(job.time['datetime']) #Get date posted of job
+    
+    #Get location of job
     try:
+        job_location = job.p.a.next_sibling.next_sibling.find('span', class_='result-hood').text
+        jobdetails['location'] = str(job_location.replace('(', '').replace(')', '')) #Clean output
+    except:
+        jobdetails['location'] = "No location given"
+    
+    joburl = job.a['href'] #Get url for job
+    jobdetails['how_to_apply'] = str(joburl)
+    
+    #Delve into each job link and get compensation and description
+    jobr = requests.get(joburl) 
+    jobsoup = BeautifulSoup(jobr.text, 'html.parser')
+    jobdetails['salary'] = str(jobsoup.find('p', class_='attrgroup').span.text) #Get compensation
+    
+    #Get description of job and clean output
+    jobdesc = jobsoup.find('section', id='postingbody') 
+    jobdetails['description'] = str(jobdesc.text.replace('\n', ' ').replace('\t', ' ')[30:]) #[30:] cleans the first 30 characters referencing a QR code
+    
+    return jobdetails
+    
+# Load a text-based configuration file
+def load_config_file():
+    argument_dictionary = []
+    file = 0
+    # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/FileIO.py
+    try:
+        filename = sys.argv[1]
         file = open(filename, "r")
-        #file_contents = file.read()
-    except FileNotFoundError:
+    except (FileNotFoundError, IndexError):
         print("File not found, it will be created.")
-        file = open(filename, "w")
-        file.write("")
-        file.close()
-	#Read each line of file and add it to a list
+        filename = os.path.join(os.path.dirname(__file__), 'cntconfig.txt')
+        file = open(filename, "w+")
+
+    #Read each line of file and add it to a list
     for aline in file:
         aline = aline.strip()
         argument_dictionary.append(aline)
@@ -115,11 +93,19 @@ def load_config_file(filename):
 def jobhunt(cursor, arg_dict):
     # Fetch jobs from website
     ## Add your code here to parse the job page
-    from bs4 import BeautifulSoup
 
     url = "https://seattle.craigslist.org/search/jjj"
-    jobdetails = fetch_new_jobs(cursor, arg_dict, url)
-	add_new_job(cursor, jobdetails)
+    r = requests.get(url) #Get page
+    soup = BeautifulSoup(r.text, 'html.parser')
+    job_links = soup.find_all('li', class_='result-row') #Find all job listings
+    for job in job_links: #Get single job
+        jobdetails = {}
+        jobdetails['id'] = job['data-pid'] #Get ID of job
+        if not cursor.execute(check_stmt, jobdetails): #Check if ID of job already exists in DB
+            jobdetails = fetch_new_job(arg_dict, job, jobdetails)
+            cursor.execute(insert_stmt, jobdetails)
+            print("Job " + jobdetails['title'] + " added.")
+            
     ## Add in your code here to check if the job already exists in the DB
 
     ## Add in your code here to notify the user of a new posting
@@ -131,10 +117,10 @@ def jobhunt(cursor, arg_dict):
 def main():
     # Connect to SQL and get cursor
     conn = connect_to_sql()
-    cursor = conn.cursor()
+    cursor = conn.cursor(buffered=True)
     create_tables(cursor, "table")
     # Load text file and store arguments into dictionary
-    arg_dict = load_config_file(sys.argv[1])
+    arg_dict = load_config_file()
     while(1):
         jobhunt(cursor, arg_dict)
         conn.commit()
